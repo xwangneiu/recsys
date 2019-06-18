32965560e98f6da506573a9e92720f0f317f885f
Minh Nguyen <m.nguyen22k@gmail.com>
2019-06-18 13:54:29 -0500

    updating user_sim function for reading the data file

--- user_similarity/movielens_user_sim_matrix_builder.py
+++ user_similarity/movielens_user_sim_matrix_builder.py
@@ -8,13 +8,13 @@ import numpy as np
 # Read the utility matrix file (derived from u.data) and 
 # transpose so the users are columns and items are rows.
 def user_similarity(data_csv, output_csv):
-	df = pd.read_csv(data_csv)
+	df = pd.read_csv(data_csv, header=0, index_col=0)
 	df = df.transpose()
 
-	df.head()
+	print(df)
 
 def main():
-    utility_matrix('../datasets/ml-100k/utility-matrix/movielens_utility_matrix', 'movielens_user_sim_matrix.csv')
+    user_similarity('../datasets/ml-100k/utility-matrix/movielens_utility_matrix.csv', 'movielens_user_sim_matrix.csv')
 
 if __name__ == '__main__':
     main()
