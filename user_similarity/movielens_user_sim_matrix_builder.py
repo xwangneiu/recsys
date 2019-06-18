cd7bdb493f8f717d3be7306c8cdbfcfe9bfaefaf
Minh Nguyen <m.nguyen22k@gmail.com>
2019-06-18 15:10:26 -0500

    Updated for both methods

--- user_similarity/movielens_user_sim_matrix_builder.py
+++ user_similarity/movielens_user_sim_matrix_builder.py
@@ -4,17 +4,28 @@
 import pandas as pd
 import numpy as np
 
-
 # Read the utility matrix file (derived from u.data) and 
 # transpose so the users are columns and items are rows.
-def user_similarity(data_csv, output_csv):
+# Returns to the matrix building callers
+def transpose_matrix(data_csv):
 	df = pd.read_csv(data_csv, header=0, index_col=0)
-	df = df.transpose()
+	df.transpose()
+	return df
 
-	print(df)
+# Builds a sim. matrix with cosine distance
+def user_similarity_cosine(data_csv, output_csv):
+	df = transpose_matrix(data_csv)
+	# Dot product of vector of two users over the magnitude 
+	# (root of the squares of vector components, multiplied with each other)
+
+
+# Builds a sim. matrix with Pearson correlation
+def user_similarity_pearson(data_csv, output_csv):
+	df = transpose_matrix(data_csv)
 
 def main():
-    user_similarity('../datasets/ml-100k/utility-matrix/movielens_utility_matrix.csv', 'movielens_user_sim_matrix.csv')
+    user_similarity_cosine('../datasets/ml-100k/utility-matrix/movielens_utility_matrix.csv', 'movielens_user_sim_matrix_cosine.csv')
+    user_similarity_pearson('../datasets/ml-100k/utility-matrix/movielens_utility_matrix.csv', 'movielens_user_sim_matrix_pearson.csv')
 
 if __name__ == '__main__':
     main()
