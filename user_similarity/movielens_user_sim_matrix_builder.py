2c6081cd286e9f664d766bfc7aea62a42f9c4cbe
Minh Nguyen <m.nguyen22k@gmail.com>
2019-06-18 13:40:52 -0500

    changing builder to include module capsulation for imports

--- user_similarity/movielens_user_sim_matrix_builder.py
+++ user_similarity/movielens_user_sim_matrix_builder.py
@@ -7,5 +7,14 @@ import numpy as np
 
 # Read the utility matrix file (derived from u.data) and 
 # transpose so the users are columns and items are rows.
-df = pd.read_csv('../datasets/ml-100k/utility-matrix/')
-df = df.transpose()
+def user_similarity(data_csv, output_csv):
+	df = pd.read_csv(data_csv)
+	df = df.transpose()
+
+	df.head()
+
+def main():
+    utility_matrix('../datasets/ml-100k/utility-matrix/movielens_utility_matrix', 'movielens_user_sim_matrix.csv')
+
+if __name__ == '__main__':
+    main()
