964ddd85450df12d71f776db1e6353bfd7880c41
Minh Nguyen <m.nguyen22k@gmail.com>
2019-06-20 15:35:22 -0500

    updating UM for yelp - failed to export Mesa,Az info

--- datasets/yelp_dataset/utility-matrix/yelp_utility_matrix_builder.py
+++ datasets/yelp_dataset/utility-matrix/yelp_utility_matrix_builder.py
@@ -8,11 +8,13 @@ import pandas as pd
 import numpy as np
 
 def utility_matrix(data_csv, output_csv):
+	
 	df_list = []
+	df = pd.DataFrame()
 	
 	# this reader was used to test the algorithm on 4 chunks
 	# reader = pd.read_csv(data_csv, iterator = True)
-	# for i in range(25):
+	# for i in range(100):
 
 	# reads yelp_business.csv to join with chunks
 	# possibly needs to be restructured to include in parameters?
@@ -21,14 +23,15 @@ def utility_matrix(data_csv, output_csv):
 	for chunk in pd.read_csv(data_csv, chunksize = 500):
 		# note that review.csv and business.csv both has 'stars' columns, which are relabeled as 'stars_x' and 'stars_y' respectively
 		chunk = pd.merge(chunk, df_business, on = 'business_id')
-		chunk = chunk[chunk.city == 'Stuttgart']
+		chunk = chunk[chunk.city == 'Mesa']
 		temp_df = pd.pivot_table(chunk, values = 'stars_x', index = 'user_id', columns = 'business_id')
 		df_list.append(temp_df)
-	df = pd.concat(df_list, sort = False)
+	while df_list:
+		df = pd.concat([df, df_list.pop(0)], sort = False)
 	df.to_csv(output_csv)
 
 def main():
-	utility_matrix('../yelp_review.csv', 'yelp_utility_matrix.csv')
+	utility_matrix('../yelp_review.csv', 'yelp_utility_matrix_mesa.csv')
 
 if __name__ == '__main__' :
 	main()
