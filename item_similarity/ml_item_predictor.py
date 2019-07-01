42285f03375ff9b451d0bc7bafd887d0fac32eb5
Jonathan Carr <rainercarr@gmail.com>
2019-07-01 08:21:45 -0500

    Preliminary coding of test driver, adapt user-based predictor for item-based prediction

---
+++ item_similarity/ml_item_predictor.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 23 13:01:47 2019
+
+@author: jonathan
+"""
+
+import numpy as np
+import pandas as pd
+import math
+import sys
+sys.path.insert(0, '../')
+import os
+import datasets
+import time
+
+def predict(dataset, dest_filename):
+    #similarity of all items to active item. INDICES OK
+    
+    print(dataset.training.sm_df)
+    sm = dataset.training.sm_df.to_numpy()
+    um = dataset.training.um_df.to_numpy()
+    sm_indices = dataset.training.sm_df.index.to_numpy()
+    sm_indices = [int(x) for x in sm_indices]
+    print(sm_indices)
+    um_indices = dataset.training.um_df.index.to_numpy()
+    um_indices = [int(x) for x in um_indices]
+    print(um_indices)
+    
+    #need to make dictionary such that keys are actual IDs, and values are positional.
+    sm_a2p_ix = {} #similarity matrix actual index -> positional index
+    sm_p2a_ix = {} #similarity matrix positional index -> actual index
+    for i in range(len(sm_indices)):
+        sm_a2p_ix[sm_indices[i]] = i
+        sm_p2a_ix[i] = sm_indices[i]
+    um_a2p_ix = {} #utility matrix actual index -> positional index
+    um_p2a_ix = {} #utility matrix positional index -> actual index
+    for i in range(len(um_indices)):
+        um_a2p_ix[um_indices[i]] = i
+        um_p2a_ix[i] = um_indices[i]
+
+    
+
+    results = pd.DataFrame(dataset.test.og_df)
+    users_and_items = dataset.test.user_item_pairs_df.to_numpy()
+    predictions = np.zeros(len(users_and_items), dtype=float)
+    
+    #this loop produces a prediction for each user/item in the users_and_items_np list
+    for i in range(len(users_and_items)):
+        print('Predicting user/item pair ' + str(i + 1))
+        
+        user = users_and_items[i][0]
+        item = users_and_items[i][1]
+        prediction = None
+        print("User: " + str(user) + " Item: " + str(item))
+        #if predictions are requested on items/users not in training set
+        user_ratings = None
+        if user not in um_a2p_ix:
+            prediction = math.nan
+        else:
+            user_ratings = um[um_a2p_ix[user], :]
+            
+            #process into dictionary ratings
+            #ratings keys are ACTUAL 
+            ratings = {}
+            for j in range(len(user_ratings)):
+                if not np.isnan(user_ratings[j]):
+                    ratings[sm_p2a_ix[j]] = user_ratings[j]
+            #print(ratings)
+            #print(user_ratings)
+            sim_item = None
+            if user not in sm_a2p_ix:
+                prediction = math.nan
+            else:     
+                sim_item = sm[sm_a2p_ix[item], :]
+                
+                similarities = {}
+                for j in range(len(sim_item)):
+                    if not np.isnan(sim_item[j]):
+                        if sm_p2a_ix[j] in ratings:
+                            similarities[sm_p2a_ix[j]] = sim_item[j]
+                #print(similarities)
+                
+                #get top 30 most similar
+                #gets np array of tuples of top 30 most similar (items|users) to active item
+                dtype = [('index', int), ('similarity', float)]
+                sim_index = np.zeros(len(similarities), dtype=dtype)
+                j = 0
+                for index, similarity in similarities.items():
+                    sim_index[j][0] = index
+                    sim_index[j][1] = similarity
+                    j += 1
+                sim_index = np.sort(sim_index, kind='heapsort', order='similarity')[::-1] #sort by similarity (descending), then reverse with [::-1] so ascending
+                top_30_most_sim = sim_index[1:31]
+                
+                
+                dtype = [('index', int), ('rating', float)]
+                
+                ratings_top_30 = np.zeros(30, dtype=dtype)
+                for j in range(len(top_30_most_sim)):
+                    ratings_top_30[j][0] = top_30_most_sim[j][0]
+                    ratings_top_30[j][1] = ratings[top_30_most_sim[j][0]]
+            
+                
+                weighted_top_30 = np.array(ratings_top_30)
+            
+        
+                #gets weighted ratings of the top 30
+                for j in range(len(top_30_most_sim)):
+                    weighted_top_30[j][1] = top_30_most_sim[j][1] * ratings_top_30[j][1]
+                '''
+                print('Top 30 similar:')
+                print(top_30_most_sim)
+                #time.sleep(3)
+                print('Top 30 ratings:' )
+                print(ratings_top_30)
+                #time.sleep(3)
+                print('Top 30 ratings, weighted:' )
+                print(weighted_top_30)
+                #time.sleep(3)
+                '''
+                #gets sum of the absolute values of the top 30 most similar items' correlation coefficients
+                #also gets sum of the weighted ratings of the top 30
+                sum_abs_correlations = 0
+                sum_weighted_ratings = 0
+                
+        #        if sum_abs_correlations == 0 or sum_weighted_ratings == 0:
+        #            predictions[i] = math.nan
+        #            continue
+        
+                for j in range(len(top_30_most_sim)):
+                    if not np.isnan(top_30_most_sim[j][1]):
+                        sum_abs_correlations += abs(top_30_most_sim[j][1])
+                    if not np.isnan(weighted_top_30[j][1]):
+                        sum_weighted_ratings += weighted_top_30[j][1]
+                        #print(sum_weighted_ratings)
+                '''
+                print('Sum of weighted ratings: ' + str(sum_weighted_ratings))
+                print('Sum of absolute correlations: ' + str(sum_abs_correlations))
+                '''
+                if sum_abs_correlations == 0 or sum_weighted_ratings == 0:
+                    prediction = math.nan
+                else:
+                    prediction = sum_weighted_ratings / sum_abs_correlations
+        print('PREDICTION for user ' + str(user) + ' on item ' + str(item) + ': ' + str(prediction))
+        predictions[i] = prediction
+        
+    predictions = pd.Series(predictions)
+        
+    results['prediction'] = predictions
+    print(results)
+    results.to_csv(dest_filename)
+    return results
+
+#adds a prediction to a test set object
+    
+def main():
+    #load MovieLens dataset
+    
+    #had to change directory, so we can run the called function in recsys/ rather than recsys/item_similarity/
+    os.chdir('..')
+    #ml_100k = datasets.load_ml_100k()
+    ml_u1 = datasets.load_ml_u1_item_pearson()
+    print(ml_u1.training.um_df)
+    os.chdir('user_similarity')
+    prediction = predict(ml_u1, 'filename')
+    print(prediction)
+    
+    #print(ml_u1.test.predictions_df)
+    #ml_u1.test.save_test_results('ml_u1_2019_06_24_test_results.csv')
+    
+    
+    
+    
+if __name__ == '__main__':
+    main()
--- test_driver.py
+++ test_driver.py
@@ -9,7 +9,8 @@ import sys
 #WHENEVER WE ADD A NEW ALGORITHM, WE NEED TO ADD PATH OF ANY PYTHON FILES WITH FUNCTIONS WE ARE USING TO THIS LIST OF PATHS
 sys.path.insert(0, '/item_similarity')
 
-import item_similarity.movielens_predictor_item as mlp_item
+import datasets
+
 
 #just asks for a user and an item to input to an algorithm
 def query_user_item():
@@ -17,16 +18,46 @@ def query_user_item():
     item = int(input("Enter item: "))
     return user, item
 
+def run_ml_test(data_source, test_source, name, data, algo, sim):
+    results_folder = str(algo) + '/'
+    if sim == 'user' or sim == 'item':
+        results_folder += '_similarity/'
+    ds = datasets.TrainingAndTest('MovieLens u1 training/test sets')
+    ds.training = datasets.Dataset(
+        name,                                              #name
+        data_source,                             #original source
+        'datasets/ml-100k/utility-matrix/' + str(name) + '_' + str(algo) + '_um.csv',    #utility matrix
+        results_folder + str(name) + '_' + str(algo) + '_sm.csv',            #similarity matrix
+        data,                                                   #data source
+        algo,                                                 #algorithm
+        sim)                                              #correlation
+    ds.test = datasets.TestSet(
+        str(name) + ' test set',                                          #name
+        test_source)
+    if algo == 'item':
+        ds.build_ml_item_predictions_df('item_similarity/' + str(name) + '_' + str(algo) + '_predictions.csv')
+    elif algo == 'user':
+        ds.build_ml_user_predictions_df('user_similarity/' + str(name) + '_' + str(algo) + '_predictions.csv')
+    elif algo == 'wnmf':
+        ds.build_ml_wnmf_predictions_df('wnmf/' + str(name) + '_' + str(algo) + '_predictions.csv')
+    print('Predictions: ')
+    print(ds.test.predictions_df)
+    ds.test.calculate_mae()
+    ds.test.calculate_rmse()
+    print("MAE: " + str(ml_u1.test.mae))
+    print("RMSE: " + str(ml_u1.test.rmse))
+    return ds
+
 def main():
     
     #main Test Driver loop: select a dataset
     run = True
     sorry = 'Sorry, test driver not implemented yet for this algorithm\n'
     print('RECOMMENDER SYSTEMS TEST DRIVER')
-    print('Get a prediction for a given user and item, using a choice of algorithms')
+    print('Get a prediction for a given dataset, using a choice of algorithms')
     while (run):
-        print('\nChoose Dataset:\n')
-        print('1--MovieLens 100k 2--Yelp (sample) 0--Quit')
+        print('\nChoose Dataset Type:\n')
+        print('1--MovieLens 100k 2--Yelp (Champaign-Urbana) 0--Quit')
         response_dataset = int(input(": "))
         
         #Quit
@@ -38,34 +69,45 @@ def main():
             run_ml = True
             
             #loading item-based matrices only once
-            utility, similarity = mlp_item.load_matrices_for_prediction('item_similarity/test_only_similarity_matrix.csv', 'datasets/ml-100k/utility-matrix/movielens_utility_matrix.csv')
-            
             while(run_ml):
-                print('MovieLens 100k Data Set:\n1--Get Actual (Observed) Rating of Film 2--Get Item-Based Prediction 3--Get User-Based Prediction 4--Get WNMF-Based Prediction 0--Back')
+                print('Select Algorithm for MovieLens 100k Data Sets:\n1--Item-Based Pearson 2--Item-Based Cosine 3--User-Based Pearson 4--User-Based Cosine 5--WNMF')
                 response_ml = int(input(": "))
-                #query actual rating of film 
+                if response_ml == 0:
+                    run_ml = False
+                    break
+                print('Select MovieLens 100k Test Set 1-5 for Prediction')
+                dataset_choice = int(input(": "))
+                if dataset_choice < 1 or dataset_choice > 5:
+                    print("Invalid dataset selection")
                 if response_ml == 1:
-                    print(sorry)
+                    algo_choice = 'item'
+                    sim_choice = 'pearson'
                 #query item-based MovieLens predictor
                 elif response_ml == 2:
-                        user, item = query_user_item()
-                        print("PREDICTION for USER " + str(user) + " on ITEM " + str(item) + ": " + str(mlp_item.predict(utility, similarity, user, item)))
-                        print('\n')
+                    algo_choice = 'item'
+                    sim_choice = 'cosine'
                 elif response_ml == 3:
-                    print(sorry)
+                    algo_choice = 'user'
+                    sim_choice = 'pearson'
                 elif response_ml == 4:
-                    print(sorry)
-                #quit
-                elif response_ml == 0:
-                    run_ml = False
-        
+                    algo_choice = 'user'
+                    sim_choice = 'cosine'
+                elif response_ml == 5:
+                    print('Not implemented yet')
+                if response_ml < 4:
+                    ds = run_ml_test('datasets/ml-100k/u' + str(dataset_choice) + '.base',  #training set source
+                                'datasets/ml-100k/u' + str(dataset_choice) + '.test',       #test set source
+                                'ml_u' + str(dataset_choice),                               #dataset name
+                                'ml',                                                       #type of data
+                                algo_choice,                                                #algorithm
+                                sim_choice)                                                 #similarity measure
+                    
         #Yelp Dataset test driver loop
         elif response_dataset == 2:
             run_yelp = True
             while(run_yelp):
                 print('\nYelp (sample):\n 1--Get Actual (Observed) Rating of Business 2--Get Item-Based Prediction 3--Get User-Based Prediction 4--Get WNMF-Based Prediction 0--Back')
                 response_yelp = int(input(": "))
-                
                 #Query actual (observed) rating of business
                 if response_yelp == 1:
                     print(sorry)
