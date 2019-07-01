189542aa3e2734253ada269bc54a0727272844f0
Minh Nguyen <m.nguyen22k@gmail.com>
2019-07-01 15:12:42 -0500

    modified Yelp UM builders to work with subsets only (no chunking), continued working on Yelp U-U predictor

--- datasets/yelp_dataset/utility-matrix/yelp_utility_matrix_builder_item.py
+++ datasets/yelp_dataset/utility-matrix/yelp_utility_matrix_builder_item.py
@@ -10,24 +10,14 @@ import numpy as np
 import json
 
 def yelp_utility_matrix(df, output_json, json_file_path = 'urbana_zip.json'):
-	with open(json_file_path, 'r') as f:
-		try:
-			zip_list = json.load(f)
-		except:
-			print('Zip file for building UM possibly empty. Returning to caller.')
-			return
-	businesses = pd.read_csv('../yelp_business.csv')
 	output_dict = {}
-	for chunk in df:
-		chunk = chunk.merge(businesses[['business_id', 'postal_code']], on = 'business_id')
-		chunk = chunk[chunk.postal_code.isin(zip_list)]
-		for j in range(len(chunk.index)):
-			# Could optmize accessing each row? iterrow() was not working at the time
-			curr = chunk.iloc[j]
-			if curr.business_id in output_dict.keys():
-				output_dict[curr.business_id][curr.user_id] = int(curr.stars)
-			else:
-				output_dict[curr.business_id] = {curr.user_id: int(curr.stars)}
+
+	for j in range(len(df.index)):
+		curr = df.iloc[j]
+		if curr.business_id in output_dict.keys():
+			output_dict[curr.business_id][curr.user_id] = int(curr.stars)
+		else:
+			output_dict[curr.business_id] = {curr.user_id: int(curr.stars)}
 	with open(output_json, 'w') as f:
 		json_dump = json.dumps(output_dict)
 		f.write(json_dump)
@@ -35,44 +25,37 @@ def yelp_utility_matrix(df, output_json, json_file_path = 'urbana_zip.json'):
 	return output_dict
 
 def main():
-	df = pd.read_csv('../yelp_review.csv', chunksize = 500)
-	yelp_utility_matrix(df, 'yelp_utility_matrix_uc_item.json')
+	df = pd.read_csv('../yelp_review_uc_training_1.csv')
+	yelp_utility_matrix(df, 'yelp_utility_matrix_uc_item_tr1.json')
 
 if __name__ == '__main__' :
 	main()
 
-# def aggregate_rewrite_matrix(data_csv):
-# 	# Take a csv, groupby users, aggregate, and fix elements
-# 	df = pd.read_csv(data_csv)
-# 	df = df.groupby(['user_id'])
-# 	df = df.agg(['sum'])
-# 	df = df.replace(to_replace = 0, value = np.nan)	
-# 	df.columns = df.columns.get_level_values(0)
-# 	df.to_csv('yelp_utility_matrix_mesa.csv')
+# def yelp_utility_matrix(df, output_json, json_file_path = 'urbana_zip.json'):
+# 	with open(json_file_path, 'r') as f:
+# 		try:
+# 			zip_list = json.load(f)
+# 		except:
+# 			print('Zip file for building UM possibly empty. Returning to caller.')
+# 			return
+# 	# businesses = pd.read_csv('../yelp_business.csv')
+# 	output_dict = {}
+# 	for chunk in df:
+# 		# chunk = chunk.merge(businesses[['business_id', 'postal_code']], on = 'business_id')
+# 		# chunk = chunk[chunk.postal_code.isin(zip_list)]
+# 		for j in range(len(chunk.index)):
+# 			# Could optmize accessing each row? iterrow() was not working at the time
+# 			curr = chunk.iloc[j]
+# 			if curr.business_id in output_dict.keys():
+# 				output_dict[curr.business_id][curr.user_id] = int(curr.stars)
+# 			else:
+# 				output_dict[curr.business_id] = {curr.user_id: int(curr.stars)}
+# 	with open(output_json, 'w') as f:
+# 		json_dump = json.dumps(output_dict)
+# 		f.write(json_dump)
+# 		f.close()
+# 	return output_dict
 
-# def utility_matrix(data_csv, output_csv):
-	
-# 	# this reader was used to test the algorithm on limited chunks
-# 	# reader = pd.read_csv(data_csv, iterator = True)
-# 	# for i in range(700):
-# 		# chunk = reader.get_chunk(300)
-
-# 	# reads yelp_business.csv to join with chunks
-# 	df_business = pd.read_csv('../yelp_business.csv')
-# 	df_business = df_business[['business_id','city']]
-
-# 	df_list = []
-# 	df = pd.DataFrame()
-
-# 	for chunk in pd.read_csv(data_csv, chunksize = 500):
-# 		chunk = pd.merge(chunk, df_business, on = 'business_id')
-# 		chunk = chunk[chunk.city == 'Stuttgart']
-# 		chunk = pd.pivot_table(chunk, values = 'stars', index = 'user_id', columns = 'business_id')
-# 		df_list.append(chunk)
-# 	# The following commented code can replace the uncommented code
-# 	# The while loop concats one chunk at a time, which can save on a little memory but requires more computational time
-# 	# del df_business
-# 	# while df_list:
-# 		# df = pd.concat([df, df_list.pop()], sort = False)
-# 	df.to_csv(output_csv)
-# 	df = pd.concat(df_list, sort = False)
+# def main():
+# 	df = pd.read_csv('../yelp_review.csv', chunksize = 500)
+# 	yelp_utility_matrix(df, 'yelp_utility_matrix_uc_item.json')
--- user_similarity/yelp_user_predictor.py
+++ user_similarity/yelp_user_predictor.py
@@ -1,6 +1,32 @@
 # SCSE RecSys Group
 # Minh N.
 # 19.07.01
-# This module finds the specified entries of a column and builds a new csv from rows including that
-# This is written to pull Urbana-Champaign businesses out from the yelp_review.csv
-# for testing against ratings on businesses with zipcodes listed in urbana_zip.json
+# This module takes a list of user-item-ratings and predicts ratings for each row based off of 
+# the similarity and utility matrix.
+
+import pandas as pd
+import numpy as np
+import math
+import json
+
+
+def load_files(training_um_path, training_sm_path, test_set_path):
+	with open(training_um_path, 'r') as f:
+		training_um = json.load(f)
+	with open(training_sm_path, 'r') as f:
+		training_sm = json.load(f)
+	test_set = pd.read_csv(test_set_path)
+	return training_um, training_sm, test_set
+
+def predict(training_um, training_sm, test_set, output_file_path):
+	
+	# give non-predictable rows math.nan
+	# drop columns so the format is user, item, observced rating, predicted.
+	return
+
+def main():
+	training_um, training_sm, test_set = load_files('../datasets/yelp_dataset/utility-matrix/yelp_utility_matrix_uc_user_tr1','','')
+	predict(training_um, training_sm, test_set, 'yelp_user_prediction_cosine.csv')
+
+if __name__ == '__main__':
+	main()
