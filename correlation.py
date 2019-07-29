ad50fb9f4a76b9f1841a663a3e95c543bb1d1bdd
Jonathan Carr <rainercarr@gmail.com>
2019-07-28 21:59:49 -0500

    Neighborhood-based run time test results

--- correlation.py
+++ correlation.py
@@ -49,24 +49,16 @@ def pearson_meanall(col1, col2):
     return corr
 
 def pearson(col1, col2):
-    #Finds corated values by checking each element of each array for non-NaN status and performing AND on the results
-    '''
-    col1_rated = np.logical_not(np.isnan(col1))
-    #print(col1_rated[0:25])
-    col2_rated = np.logical_not(np.isnan(col2))
-    #print(col2_rated[0:25])
-    corated = np.logical_and(col1_rated, col2_rated)
-    '''
+
+    #multiplies both columns by one another, then divides by one of them (this makes all non-corated elements into nan)
     col1_new = (col1 * col2) / col2
     col2_new = (col2 * col1) / col1
     col1 = col1_new
     col2 = col2_new
+    
     #removes nans from both columns, leaving only corated items
     col1 = col1[~np.isnan(col1)]
-    col2 = col2[~np.isnan(col2)]
-    print(col1)
-    print(col2)
-
+    col2 = col2[~np.isnan(col2)]    
     
     #if there are no corated values, return 0 to save time
     if len(col1) == 0: #this is a sum of True values (each True == 1)
@@ -75,31 +67,11 @@ def pearson(col1, col2):
     sum_product_distances_from_mean = 0
     sum_squared_col1_distances_from_mean = 0
     sum_squared_col2_distances_from_mean = 0
+    
     #get mean values of columns before removing non-corated user ratings
-    '''
-    def remove_non_corated(col):
-        for i in range(len(col)):
-            if corated[i] == False:
-                col[i] = math.nan
-        return col
-    '''
-    #remove non-corated values by multiplying them times corated (n * True = n, n * False = 0)
-    '''
-    col1 = col1 * corated
-    col2 = col2 * corated
-    '''
     col1_mean = np.mean(col1) #mean excluding nans
     col2_mean = np.mean(col2)
-    '''
-    def final_calc(i):
-        col1_distance_from_mean = col1[i] - col1_mean
-        col2_distance_from_mean = col2[i] - col2_mean
-        sum_product_distances_from_mean += col1_distance_from_mean * col2_distance_from_mean
-        #denominator of formula
-        sum_squared_col1_distances_from_mean += (col1_distance_from_mean) ** 2
-        sum_squared_col2_distances_from_mean += (col2_distance_from_mean) ** 2
-    [final_calc(i) for i in col1 if corated[i]]
-    '''    
+
     for i in range(len(col1)):
         if col1[i] != math.nan:
             #numerator of formula
@@ -149,6 +121,7 @@ def cos_old(col1, col2):
     '''
     return np.dot(col1, col2) / (np.linalg.norm(col1, ord=2) * np.linalg.norm(col2, ord=2))
 
+#testing using timeit to improve performance: this code may be reused
 def operation_time():
     t = timeit.Timer(stmt='''
 col1 = col1 * corated
