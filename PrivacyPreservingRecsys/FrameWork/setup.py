5d0c7f13c8f5e84de37c0af619a3cd4fd9aea991
Novak <cuilongyin@users.noreply.github.com>
2019-10-27 14:38:12 -0400

    Update setup.py

--- PrivacyPreservingRecsys/FrameWork/setup.py
+++ PrivacyPreservingRecsys/FrameWork/setup.py
@@ -3,3 +3,5 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(name = 'myAlgo', ext_modules = cythonize("myAlgorithm.pyx"))
+
+# add build_ext --inplace when you run this file.
